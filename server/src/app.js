328284f3205c72b034c42f0f039a7140a92c8df5
Shivam-programs <shivamyadav13082004@gmail.com>
2026-07-25 17:15:13 +0530

    initial commit

--- server/src/app.js
+++ server/src/app.js
@@ -1,15 +1,16 @@
 import express from "express";
+import dotenv from "dotenv";
 import http from "http";
 import cors from "cors";
 import { Server } from "socket.io";
 
+dotenv.config();
 const app = express();
 app.use(cors());
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173",
-        methods: ["GET", "POST"],
+        origin: process.env.FRONTEND_URL,
     },
 });
 
@@ -28,6 +29,8 @@ io.on("connection", (socket) => {
     });
 });
 
-server.listen(5000, () => {
-    console.log("Server running on port 5000");
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
 });
