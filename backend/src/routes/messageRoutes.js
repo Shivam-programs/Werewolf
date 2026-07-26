4b97d856960c316cf8ccdda3365a260be8825fa5
Shivam-programs <shivamyadav13082004@gmail.com>
2026-07-26 22:02:18 +0530

    msg route updated

--- backend/src/routes/messageRoutes.js
+++ backend/src/routes/messageRoutes.js
@@ -1,13 +1,3 @@
-// import express from "express";
-// import { getMessages,getPlayers,sendMessage } from "../controllers/messageController.js";
-
-// const router = express.Router();
-// router.get("/players/:roomCode", getPlayers);
-// router.get("/messages/:roomCode", getMessages);
-// router.post("/messages", sendMessage);
-
-// export default router;
-
 import express from "express";
 import {
     getMessages,
